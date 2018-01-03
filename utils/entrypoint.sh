64cfffc23ab0ca1ab2b26f2caa73583aa0f5125a
Hleb Rubanau <g.rubanau@gmail.com>
2018-01-03 12:06:12 +0000

    more bugfixes

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -39,25 +39,28 @@ for domain in $LE_DOMAINS ; do
     DOMAINS_LIST="$DOMAIN_LIST -d $domain"
 done
 
-say "Running nginx in background"
-envsubst '$CERTBOT_WEBROOT $CERT_NAME' < /usr/share/nginx/ssl_params.template > /etc/nginx/ssl_params
-envsubst '$CERT_NAME' < /opt/nginx-le/certbot_live_renew.sh > /usr/local/bin/certbot_live_renew
-nginx 
 
 if [ ! -e $EXPECTED_CERTPATH ]; then
     say "Provisioning certificates from letsencrypt"
+    envsubst '$CERTBOT_WEBROOT $CERT_NAME' < /usr/share/nginx/ssl_params.template | grep -v ssl > /etc/nginx/ssl_params
+    say "Running nginx in background"
+    nginx
     certbot certonly $CERTBOT_FLAGS \
             --agree-tos -m $LE_EMAIL \
             --webroot -w $CERTBOT_WEBROOT \
             --cert-name $CERT_NAME        \
             $DOMAINS_LIST
+    envsubst '$CERTBOT_WEBROOT $CERT_NAME' < /usr/share/nginx/ssl_params.template > /etc/nginx/ssl_params
 else
     say "Trying to renew certificates"
+    envsubst '$CERTBOT_WEBROOT $CERT_NAME' < /usr/share/nginx/ssl_params.template > /etc/nginx/ssl_params
+    nginx
     certbot renew $CERTBOT_FLAGS --cert-name $CERT_NAME 
 fi
 
 nginx -s stop
 
+envsubst '$CERT_NAME' < /opt/nginx-le/certbot_live_renew.sh > /usr/local/bin/certbot_live_renew
 chmod u+x /usr/local/bin/certbot_live_renew
 
 unset CERT_NAME EXPECTED_CERPATH DOMAIN_OPTS CERTBOT_WEBROOT LE_MAIL LE_DOMAINS
