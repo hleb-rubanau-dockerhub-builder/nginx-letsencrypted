d0971345676bbf5af60b313286365cca3122cd54
Hleb Rubanau <g.rubanau@gmail.com>
2018-05-07 10:21:52 +0300

    add --allow-subset-of-names to live renewal script

--- utils/certbot_live_renew.sh
+++ utils/certbot_live_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # it is a template
-exec certbot renew ${CERTBOT_FLAGS} --post-hook reload_nginx $@
+exec certbot renew ${CERTBOT_FLAGS} --allow-subset-of-names --post-hook reload_nginx $@
