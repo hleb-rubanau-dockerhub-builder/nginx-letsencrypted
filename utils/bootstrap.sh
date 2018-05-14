92d85d62f6392a18a2c83212696838561f906fb1
Hleb Rubanau <g.rubanau@gmail.com>
2018-05-14 13:11:05 +0300

    bootstrap script drafted

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -4,115 +4,63 @@ set -e
 
 source /usr/local/share/helper_functions.sh
 
-# if failure was tracked during grace period
-function letsencrypt_failed_recently() {
-    [ -e $LETSENCRYPT_FAILURE_LOG_FILE ] && check_file_params $LETSENCRYPT_FAILURE_LOG_FILE -mmin -${LETSENCRYPT_FAILURE_GRACE_PERIOD}
-}
-
 require_var PRIMARY_DOMAIN
 
-LE_DOMAINS="$( get_domains_list.sh )"
+DESIRED_DOMAINS_LIST="$( get_domains_list.sh )"
 say "Full domains list [AUTOFILL_DOMAINS=${AUTOFILL_DOMAINS:-false}]: $LE_DOMAINS"
 
 export SSL_CERT_MODE=${SSL_CERT_MODE:-staging}
-source /usr/local/bin/determine_cert_paths
-source /usr/local/bin/determine_cert_writeability
-source /usr/local/bin/determine_cert_status
+source /usr/local/bin/determine_cert_strategy
 
-report_ssl_cert_status
+report_ssl_cert_strategy
 
-if [ "$SSL_CERT_STATUS" != "actual" ]; then
-    if [ "$SSL_FILES_ARE_WRITABLE" != "true" ]; then
-        say "WARNING: certificates status is $SSL_CERT_STATUS , but certificates are not writable" 
-        if [ "$SSL_CERT_MODE" != "snakeoil" ] && [ "$SSL_ALLOW_FAILOVER_TO_SNAKEOIL" == "true" ]; then
-            say "WARNING: trying snakeoil failover"
-            export SSL_CERT_MODE=snakeoil
-            exec $0 $*
-        fi
-
-        if [ "$SSL_CERT_STATUS" != "missing" ] && [ "$SSL_ALLOW_OUTDATED_CERTIFICATES" == "true" ]; then
-            say "WARNING: proceeding with"
-        fi
-
+if [ "$SSL_CERT_ACCEPTED_STRATEGY" = "fail" ]; then
+    if [ "$SSL_TRY_TO_FAILOVER_ON_ERRORS" != "true" ]; then    
+        die "Cannot proceed with $SSL_CERT_DESIRED_STRATEGY"
     fi
-fi
-
-
-case "$SSL_CERT_STATUS" in 
-    missing) 
-    ;;
-    outdated
-
-[ "$SSL_CERT_STATUS" ]
-## now we need to make decisions
-if [ "$SSL_FILES_ARE_PRESENT"!="true" ]; then
-
-fi
-
-if [ "$CERT_MODE" = "snakeoil" ]; then
-    provision_snakeoil_certs $PRIMARY_DOMAIN $LE_DOMAINS
-elif [ "$LETSENCRYPT_NEEDS_FAILOVER" != "yes" ]; then
-    provision_letsencrypt_certs $PRIMARY_DOMAIN $LE_DOMAINS
-
-    say "Provision/update letsencrypt certs ($CERT_MODE)"
-	if [ ! -e $SSL_CERTPATH ]; then
-	    enforce_webroot_only_mode 
-	else 
-	    enable_certs_mode
-	fi
     
-    DOMAINS_LIST=""
-    for domain in $LE_DOMAINS ; do DOMAINS_LIST="$DOMAINS_LIST -d $domain" ; done
-
-    nginx -t || die "Nginx misconfiguration"
-    say "Running nginx in background"
-    nginx
-    say "Calling certbot"
-    
-    certbot certonly $CERTBOT_FLAGS \
-        --agree-tos -m $LE_EMAIL \
-        --webroot -w $CERTBOT_WEBROOT \
-        $DOMAINS_LIST   \
-    || CERTBOT_FAILED="yes"
-
-    if [ "$CERTBOT_FAILED" == "yes" ]; then
-        msg="Generation failed for $LE_DOMAINS"
-        say "ERROR: $msg"
-        echo "[$(date +%F-%T)] FAILURE (mode=$CERT_MODE, domains: $LE_DOMAINS )" >> $LETSENCRYPT_FAILURE_LOG_FILE
-    fi
-
-	say "Stopping nginx"
-	kill -TERM $(cat /var/run/nginx.pid) 
-	#nginx -s stop
-	while [ -f /var/run/nginx.pid ]; do
-	  say "Waiting for nginx to shut down"
-	  sleep 2;
-	done
-
-	say "Nginx stopped"
+    # attempts to failover
         
-
-    if [ "$CERTBOT_FAILED" == "yes" ]; then maybe_failover_to_snakeoil ; fi
-
+    # if SOME certs are in place, just use them
+    if [ "$SSL_CERT_STATUS" != "missing" ]; then
+        say "WARNING: some errors found. Proceeding with existing certificates, not applying action '"$SSL_CERT_DESIRED_STRATEGY"'"
+        export SSL_CERT_ACCEPTED_STRATEGY="proceed"
+    elif [ "$SSL_CERT_MODE" != "snakeoil" ]; then
+        say "WARNING: certificates are missing, and letsencrypt cannot proceed. Trying snakeoil failover"
+        export SSL_CERT_MODE="snakeoil"
+        exec $0 $*
+    else
+        die "Certificates are missing, and cannot be generated, and there are no failover strategies left. Giving up."
+    fi
 fi
 
+provision_renewal_script $SSL_CERT_MODE
 
-say "Enabling certs mode and testing"	
-enable_certs_mode
-nginx -t
-
-say "Clean up environment"
-unset CERT_NAME SSL_CERTPATH DOMAIN_OPTS CERTBOT_WEBROOT LE_MAIL LE_DOMAINS \
-      SNAKEOIL_COMPANY_DEPT SNAKEOIL_COMPANY_NAME SNAKEOIL_COMPANY_CITY SNAKEOIL_COMPANY_COUNTRY \
-      LETSENCRYPT_FAILURE_GRACE_PERIOD LETSENCRYPT_FAILURE_LOG_FILE LETSENCRYPT_FAILOVER_TO_SNAKEOIL
-
-set -x
-say "Entrypoint is over, passing execution to CMD ($@)"
-if [ "$@" = "nginx -g \"daemon off;\"" ]; then
-    # for some reason interpolation of quotes did not work correctly
-    say "Running nginx command explicitly"
-    exec "nginx -g 'daemon off;'"
+if [ "$SSL_CERT_MODE" = "snakeoil" ]; then
+    tweak_nginx_configs force_snakeoil_mode 
 else
-    say "Running CMD"
-    exec $@
+    tweak_nginx_configs force_letsencrypt_mode
+fi 
+
+if [ "$SSL_CERT_DESIRED_STRATEGY" = "generate" ] || [ "$SSL_CERT_DESIRED_STRATEGY" = "renew" ]; then
+    if [ "$CERT_MODE" = "snakeoil" ]; then
+        provision_snakeoil_certs $PRIMARY_DOMAIN $LE_DOMAINS
+    else
+        provision_letsencrypt_certs $PRIMARY_DOMAIN $LE_DOMAINS || LE_FAILURE="true"
+        if [ "$LE_FAILURE"  = "true" ] ; then
+            if [ "$SSL_TRY_TO_FAILOVER_ON_ERRORS" ]; then
+                source /usr/local/bin/determine_cert_status
+                if [ "$SSL_CERT_STATUS" != "missing" ]; then
+                    say "WARNING: some errors occured during letsencrypt generation; however, we will proceed"
+                else
+                    say "WARNING: some errors happened during letsencrypt generation, and cert file(s) are missing -- switching to snakeoil mode"
+                    export SSL_CERT_MODE=snakeoil
+                    exec $0 $*
+                fi
+            fi
+        fi
+    fi
 fi
+
+
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
